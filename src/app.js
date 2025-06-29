2e9a921cd40058c5863cc111f02fc60b46acffdf
Bbenrod <benjamintec3455@gmail.com>
2025-06-28 20:46:03 -0600

    Basic page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
+
+const path = require("path");
 const express = require("express");
 const router = require("./routes");
+
 const app = express();
-const port = process.env.PORT || 3000;
+const rootDir = path.resolve(__dirname, "..");
 
-app.use("/", router);
+const PORT = process.env.PORT || 3000;
+const BLOG_ROUTE = process.env.BLOG_ROUTE || "/blog";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.set("view engine", "ejs");
+app.set("views", path.join(rootDir, "src", "views"));
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(BLOG_ROUTE, router);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
